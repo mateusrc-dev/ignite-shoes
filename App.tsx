c18d1aae67b3de9f3a14ed7dee82d66a796e887a
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-10 12:33:45 -0300

    updated project

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import OneSignal from 'react-native-onesignal';
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('15b635aa-1c82-4b15-b4d1-2ca3d805ef61')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
