1d7db3f987b4d17c5330ce131bea0c258b7fdbd8
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-11 23:14:18 -0300

    updated project

--- App.tsx
+++ App.tsx
@@ -1,33 +1,46 @@
-import { useEffect } from 'react';
-import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal';
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { useEffect, useState } from "react";
+import OneSignal, {
+  NotificationReceivedEvent,
+  OSNotification,
+} from "react-native-onesignal";
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
-import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+import { Notification } from "./src/components/Notification";
+import { tagUserInfoCreate } from "./src/notifications/notificationsTags";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
 
-OneSignal.setAppId('15b635aa-1c82-4b15-b4d1-2ca3d805ef61')
+OneSignal.setAppId("15b635aa-1c82-4b15-b4d1-2ca3d805ef61");
 
-OneSignal.setEmail('mateus@email.com')
+OneSignal.setEmail("mateus@email.com");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [notification, setNotification] = useState<OSNotification>();
 
-  tagUserInfoCreate()
+  tagUserInfoCreate();
 
   useEffect(() => {
-    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
-      console.log(notificationReceivedEvent);
-    })
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler(
+      (notificationReceivedEvent: NotificationReceivedEvent) => {
+        const response = notificationReceivedEvent.getNotification();
+
+        setNotification(response);
+      }
+    );
 
     return () => unsubscribe;
-  }, [])
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -39,6 +52,13 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
+      {notification?.title && (
+        <Notification
+          title={notification.title}
+          onClose={() => setNotification(undefined)}
+        />
+      )}
     </NativeBaseProvider>
   );
 }
